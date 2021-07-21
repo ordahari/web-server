3eac1aa5ef1c1a2ab47d03e8dae238afcbf96392
or <ordahari@gmail.com>
2021-07-21 19:34:51 +0300

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,25 @@
-const weatherForm =document.querySelector('form')
-const searchInput =document.querySelector('input')
+const weatherForm = document.querySelector('form')
+const searchInput = document.querySelector('input')
 
 const messageOne = document.querySelector('#message-1')
 const messageTow = document.querySelector('#message-2')
 
-
-weatherForm.addEventListener('submit',(e)=>{
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    messageOne.textContent ='Loading...'
-    messageTow.textContent =''
+    messageOne.textContent = 'Loading...'
+    messageTow.textContent = ''
+    const url = '/weather?address=';
 
-const url = 'http://localhost:3000/weather?address=';
+    fetch(url + searchInput.value).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            }
+            else {
+                messageOne.textContent = data.forecast;
+                messageTow.textContent = data.location;
+            }
+        })
 
-fetch(url+searchInput.value).then((response)=>{
-
-    response.json().then((data)=>{
-        if(data.error){
-            messageOne.textContent =data.error
-        }
-        else{
-            messageOne.textContent =data.forecast;
-            messageTow.textContent =data.location;
-        }
-    
     })
-
-})
-
-
-   // console.log(searchInput.value)
 })
