21cea21ec0e5c4d86f6c775247f1666e140f3dca
or <ordahari@gmail.com>
2021-07-23 09:34:11 +0300

    add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,12 +11,10 @@ const forecast = (latitude, longitude, callback) => {
         }
         else {
 
-            callback(undefined,body.current.weather_descriptions[0]+', it is currently '+body.current.temperature+' degrees out, feels like '+body.current.feelslike+' degrees.')
-            // callback(undefined, {
-            //     weather_descriptions: body.current.weather_descriptions[0],
-            //     temperature: body.current.temperature,
-            //     feelslike: body.current.feelslike
-            // })
+           callback(undefined,body.current.weather_descriptions[0]+ '. It is currently '+
+           body.current.temperature+' degrees out. It feels like '+ body.current.feelslike +
+            ' degrees out. The humidity is '+ body.current.humidity+'%'
+           )
         }
     })
 }
